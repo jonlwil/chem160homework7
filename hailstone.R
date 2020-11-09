6857f8530736106296411bceb6db28cdff522d52
jonlwil <70457126+jonlwil@users.noreply.github.com>
2020-11-08 23:52:29 -0800

    Add files via upload

---
+++ hailstone.R
@@ -0,0 +1,15 @@
+seqlen<-c()
+for(i in 1:1000) {
+  n=i
+  counter=1
+  while (n != 1) {
+    cat(n,"\n")
+    if (n%%2==0) {
+      n<-as.integer(n/2)
+    } else {
+      n<-as.integer(3*(n+1))
+      counter=+1
+    }
+  }
+}
+plot(seqlen)
